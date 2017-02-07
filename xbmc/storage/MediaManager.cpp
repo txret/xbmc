25540cd8ad84bc98ae88a166a0084acd430e0c39
Sam Nazarko <email@samnazarko.co.uk>
2017-02-07 17:49:18 +0000

    Don't notify when /boot is mounted. We do this when /boot is first accessed (systemd-auto), which can cause a spurious notification
    Signed-off-by: Sam Nazarko <email@samnazarko.co.uk>

--- xbmc/storage/MediaManager.cpp
+++ xbmc/storage/MediaManager.cpp
@@ -687,6 +687,8 @@ std::vector<std::string> CMediaManager::GetDiskUsage()
 
 void CMediaManager::OnStorageAdded(const MEDIA_DETECT::STORAGE::StorageDevice& device)
 {
+if (device.path == "/boot")
+  return;
 #ifdef HAS_DVD_DRIVE
   const std::shared_ptr<CSettings> settings = CServiceBroker::GetSettingsComponent()->GetSettings();
   if (settings->GetInt(CSettings::SETTING_AUDIOCDS_AUTOACTION) != AUTOCD_NONE || settings->GetBool(CSettings::SETTING_DVDS_AUTORUN))
