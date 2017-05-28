92e3836b62357b0b508be0d1dc3d14d23dd93818
Greg McCarthy <greg@gjmccarthy.co.uk>
2017-05-28 09:42:27 +0100

    Hack to force rapidjson version

--- cmake/modules/FindRapidJSON.cmake
+++ cmake/modules/FindRapidJSON.cmake
@@ -58,15 +58,7 @@ if(PKG_CONFIG_FOUND)
   pkg_check_modules(PC_RapidJSON RapidJSON>=1.0.2 QUIET)
 endif()
 
-if(CORE_SYSTEM_NAME STREQUAL windows OR CORE_SYSTEM_NAME STREQUAL windowsstore)
   set(RapidJSON_VERSION 1.1.0)
-else()
-  if(PC_RapidJSON_VERSION)
-    set(RapidJSON_VERSION ${PC_RapidJSON_VERSION})
-  else()
-    find_package(RapidJSON 1.1.0 CONFIG REQUIRED QUIET)
-  endif()
-endif()
 
 find_path(RapidJSON_INCLUDE_DIR NAMES rapidjson/rapidjson.h
                                 PATHS ${PC_RapidJSON_INCLUDEDIR})
